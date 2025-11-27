815efc2443229df93c981c24d0613b3eff52f603
Oscar <altepommes04@gmail.com>
2025-11-27 18:17:59 +0100

    Footer Upate

--- server/php/html-structure/footer.php
+++ server/php/html-structure/footer.php
@@ -1,21 +1,5 @@
 <p id="copyright">© Metis 2025</p>
 <p id="help">concact <span id="highlight">oscar-streich@t-online.de</span> for help</p>
-
-<!--<div id="socialMedia">
-    <div class="oscar">
-        <a target="_blank" href="https://www.instagram.com/oscar_f20001/">
-            <i class="fa-brands fa-instagram"></i>
-            <p>@oscar_f20001</p>
-        </a>
-    </div>
-    <div class="rapha">
-        <a target="_blank" href="https://www.instagram.com/rap.haelo/">
-            <i class="fa-brands fa-instagram"></i>
-            <p>@rap.haelo</p>
-        </a>
-    </div>
-</div>-->
-
 <p id="bedingungen">
     <a href="/../../client/bedingungen.php">Bedingungen</a>
 </p>
